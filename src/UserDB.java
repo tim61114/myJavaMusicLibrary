5d2661c541f8405369962e7d00fd181fbef95895
Tim Fan <tim61114@gmail.com>
2021-12-13 15:58:14 -0800

    added removeUser for admin

--- src/UserDB.java
+++ src/UserDB.java
@@ -1,13 +1,18 @@
+import java.io.File;
+
 public class UserDB extends Database{
+    private int userCount;
 
 
     public UserDB(String dbName) {
         super(dbName);
     }
 
+
     public int getNumUsers(){
-        return countRows("users") - 1;
+        return IDQuery("select id from users order by id desc limit 1");
     }
+
     public boolean createNewUser(String username, String password){
         Query("insert into users values(" + (getNumUsers() + 1) + ",'"+username+"','"+password+"','"+username+".db',false)");
         return true;
@@ -18,6 +23,24 @@ public class UserDB extends Database{
             return false;
         }
         else return true;
+    }
+
+    public String matchPassword(String username){
+        return singleQuery("select password from users where username = '" + username + "'").get(0);
+    }
+
+    //admin method
+    public boolean removeUser(String username){
+        if(!username.equals("admin")){
+            String sql = "delete from users where username = '"+username+"'";
+            Query(sql);
+            File file = new File("data/"+username);
+            for(File subFile: file.listFiles()){
+                subFile.delete();
+            }
+            file.delete();
+            return true;
+        }else return false;
     }
 
 
