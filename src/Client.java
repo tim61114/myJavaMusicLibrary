19b62b0bb6155d895b7415924692f4879072f9d7
Tim Fan <tim61114@gmail.com>
2021-12-13 16:06:05 -0800

    added adminMenu and added import songs

--- src/Client.java
+++ src/Client.java
@@ -21,7 +21,7 @@ public class Client {
         File file = new File(dataPath+username+"/"+username+".db");
         try {
             file.createNewFile();
-            MusicDB mdb = new MusicDB(file.getPath());
+            MusicDB mdb = new MusicDB(file.getPath(),true);
             mdb.init();
         } catch (IOException e) {
             System.out.println("Error: Unable to create user database.");
@@ -29,12 +29,57 @@ public class Client {
     }
 
     public boolean login(String username, String password){
-        return password.equals(userDB.singleQuery("select password from users where username = '" + username + "'").get(0));
+        return password.equals(userDB.matchPassword(username));
     }
 
     public Library loadUserMusicDB(String username){
         MusicDB mDB = new MusicDB(dataPath+username+"/"+username+".db");
         return new Library(mDB);
+    }
+
+    public void adminMenu(){
+        int input = 0;
+        String inputString;
+        Scanner scanner = new Scanner(System.in);
+        while(input != 3){
+            System.out.println("Admin Menu:");
+            System.out.println("1. Retrieve password for user");
+            System.out.println("2. Remove User");
+            System.out.println("3. Logout");
+            input = scanner.nextInt();
+            switch(input){
+                case 1:
+                    System.out.println("Please enter the username");
+                    String username = scanner.next();
+                    if(userDB.checkUsernameExists(username)){
+                        System.out.println("Retrieve password for user '"+username+"'.");
+                        System.out.println("Enter password");
+                        inputString = scanner.next();
+                        if(inputString.equals(userDB.matchPassword("admin"))){
+                            System.out.println("Password for user '"+username+"': "+userDB.matchPassword(username)+"\n");
+                        }
+                    } else {
+                        System.out.println("This user does not exist.\n");
+                    }
+
+                    break;
+
+                case 2:
+                    System.out.println("Please enter the username");
+                    String deleteUsername = scanner.next();
+                    System.out.println("Delete user '"+deleteUsername+"'");
+                    System.out.println("Enter password");
+                    inputString = scanner.next();
+                    if(inputString.equals(userDB.matchPassword("admin"))){
+                        if(userDB.removeUser(deleteUsername)){
+                            System.out.println("Deletion Success.");
+                        } else {
+                            System.out.println("Deletion Failed.");
+                        }
+                    }
+                    break;
+            }
+        }
     }
 
     public static void showLib(Library lib){
@@ -72,92 +117,140 @@ public class Client {
         String stringInput;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, " + currentUser);
-        while(input != 9){
+        while(input != 10){
             System.out.println("\nMenu: ");
             System.out.println("1. My library");
             System.out.println("2. Shuffle");
-            System.out.println("3. Import songs (Coming soon)");
+            System.out.println("3. Import songs (from album)");
             System.out.println("4. Create new Playlist");
             System.out.println("5. Export my playlists");
             System.out.println("6. Play next song");
             System.out.println("7. View now playing list");
             System.out.println("8. Play/Pause");
-            System.out.println("9. Log out");
+            System.out.println("9. What am I listening?");
+            System.out.println("10. Logout");
             System.out.println();
             if(lib.nowPlaying != null){
-                if(pause){
+                lib.nowPlaying.play(pause);
+                /*if(pause){
                     System.out.println("Now playing "+ lib.nowPlaying.showSong() + " (Paused)");
                 } else {
                     System.out.println("Now playing "+ lib.nowPlaying.showSong());
-                }
+                }*/
             }
 
             input = scanner.nextInt();
-
-            switch(input){
-                case 1:
-                    showLib(lib);
-                    break;
-                case 2:
-                    lib.Shuffle();
-                    lib.nowPlaying = lib.nowPlayingList.get(lib.nowPlayingIndex);
-                    break;
-                case 4:
-                    lib.showSongsWithIndex();
-                    System.out.println("Please enter the index of the songs you want to add into the playlist in a line.");
-                    scanner.nextLine();
-                    stringInput = scanner.nextLine();
-                    String[] splited = stringInput.split(" ");
-                    List<Integer> indexes = new ArrayList<>();
-                    for(String s: splited){
-                        indexes.add(Integer.parseInt(s));
-                    }
-                    Collections.sort(indexes);
-                    if(indexes.get(indexes.size() - 1) > lib.songs.size()){
-                        System.out.println("Sorry, you do not have more than "+indexes.get(indexes.size() - 1) + " songs.");
-                    }
-                    else{
-                        System.out.println("Please name your playlist");
-                        stringInput = scanner.next();
-                        Playlist temp = lib.createNewPlaylist(stringInput,indexes);
-                        temp.show();
-                    }
-                    break;
-                case 5:
-                    lib.exportPlaylists(currentUser);
-                    break;
-                case 6:
-                    if(lib.nowPlayingList.isEmpty() || lib.nowPlayingIndex == -1){
-                        System.out.println("Play a song!");
-                    } else if(lib.nowPlayingIndex + 1  == lib.nowPlayingList.size()){
-                        lib.nowPlayingIndex = -1;
-                        lib.nowPlaying = null;
-                        lib.nowPlayingList = new ArrayList<>();
-                        System.out.println("Play a new list!");
-
-                    } else {
-                        lib.nowPlayingIndex++;
-                        lib.nowPlaying = lib.nowPlayingList.get(lib.nowPlayingIndex);
-                    }
-                    break;
-                case 7:
-                    if(lib.nowPlayingList.isEmpty()){
-                        System.out.println("I'll talk if you play something first!");
-                    } else {
-                        System.out.println();
-                        System.out.println("Current playing list:");
-                        for(Song s:lib.nowPlayingList){
-                            System.out.println(s.showSong());
+            try{
+                switch(input){
+                    case 1:
+                        showLib(lib);
+                        break;
+                    case 2:
+                        if(lib.Shuffle() == null){
+                            System.out.println("I see no songs right here hmm...");
+                            break;
                         }
-                    }
-                    break;
-                case 8:
-                    if(lib.nowPlaying == null){
-                        System.out.println("Play something first!");
-                    } else {
-                        pause = !pause;
-                    }
-                    break;
+                        lib.nowPlaying = lib.nowPlayingList.get(lib.nowPlayingIndex);
+                        break;
+                    case 3:
+                        try{
+                            System.out.println("This helper will let you import songs given the name of the artist and the name of the album.");
+                            System.out.println("Please enter the name of the Artist");
+                            //String a = scanner.nextLine();
+                            scanner.nextLine();
+                            String artistName = scanner.nextLine();
+                            System.out.println("Please enter the name of the Album");
+                            String albumName = scanner.nextLine();
+                            DataFetcher df = new DataFetcher();
+                            Parser p = new Parser();
+                            String[] data = df.fetchAlbumID(artistName,albumName);
+                            String yesOrNo = scanner.next();
+                            if(yesOrNo.equalsIgnoreCase("y")){
+                                List<Song> newAlbum = p.getFullAlbum(data);
+                                System.out.println("Here's what I got for you.");
+                                newAlbum.forEach(song -> System.out.println(song.showSong()));
+                                System.out.println("Updating database...");
+                                lib.addNewAlbumToDB(newAlbum,data[2],data[1]);
+                                System.out.println("Done.");
+                            }
+                        } catch (IndexOutOfBoundsException e){
+                            System.out.println("Sorry, I can't find this album.");
+                        }
+                        break;
+                    case 4:
+                        if(!lib.songs.isEmpty()){
+                            lib.showSongsWithIndex();
+                            System.out.println("Please enter the index of the songs you want to add into the playlist in a line.");
+                            scanner.nextLine();
+                            stringInput = scanner.nextLine();
+                            String[] splited = stringInput.split(" ");
+                            List<Integer> indexes = new ArrayList<>();
+                            for(String s: splited){
+                                indexes.add(Integer.parseInt(s));
+                            }
+                            Collections.sort(indexes);
+                            if(indexes.get(indexes.size() - 1) > lib.songs.size()){
+                                System.out.println("Sorry, you do not have more than "+indexes.get(indexes.size() - 1) + " songs.");
+                            }
+                            else{
+                                System.out.println("Please name your playlist");
+                                stringInput = scanner.next();
+                                if(stringInput.matches("[a-zA-Z1-9']+")){
+                                    Playlist temp = lib.createNewPlaylist(stringInput,indexes);
+                                    temp.show();
+                                } else{
+                                    System.out.println("No symbols naughty boy");
+                                }
+                            }
+                        } else {
+                            System.out.println("You gotta add some songs first!");
+                        }
+
+                        break;
+                    case 5:
+                        if(!lib.playlists.isEmpty()) lib.exportPlaylists(currentUser);
+                        else System.out.println("Ok I exported nothing. You have no playlists!");
+                        break;
+                    case 6:
+                        if(lib.nowPlayingList.isEmpty() || lib.nowPlayingIndex == -1){
+                            System.out.println("Play a song!");
+                        } else if(lib.nowPlayingIndex + 1  == lib.nowPlayingList.size()){
+                            lib.nowPlayingIndex = -1;
+                            lib.nowPlaying = null;
+                            lib.nowPlayingList = new ArrayList<>();
+                            System.out.println("Play a new list!");
+
+                        } else {
+                            lib.nowPlayingIndex++;
+                            lib.nowPlaying = lib.nowPlayingList.get(lib.nowPlayingIndex);
+                        }
+                        break;
+                    case 7:
+                        if(lib.nowPlayingList.isEmpty()){
+                            System.out.println("I'll talk if you play something first!");
+                        } else {
+                            System.out.println();
+                            System.out.println("Current playing list:");
+                            for(Song s:lib.nowPlayingList){
+                                System.out.println(s.showSong());
+                            }
+                        }
+                        break;
+                    case 8:
+                        if(lib.nowPlaying == null){
+                            System.out.println("Play something first!");
+                        } else {
+                            pause = !pause;
+                        }
+                        break;
+                    case 9:
+                        lib.showBio();
+                        break;
+                }
+            }catch(NumberFormatException e){
+                System.out.println("As you wish.");
+            }catch(InputMismatchException e){
+                System.out.println("Input numbers not letters!");
             }
         }
     }
@@ -184,9 +277,12 @@ public class Client {
                     pwd = scanner.next();
                     if(!client.login(username,pwd)) System.out.println("Wrong password");
                 }while(!client.login(username,pwd));
-                currentUser = username;
-                libMenu(client.loadUserMusicDB(username));
-
+                if(username.equals("admin")){
+                    client.adminMenu();
+                } else {
+                    currentUser = username;
+                    libMenu(client.loadUserMusicDB(username));
+                }
             } else if (input == 2){
                 do{
                     System.out.println("Please enter username");
@@ -201,7 +297,7 @@ public class Client {
                     if(!pwd.equals(pwdCheck)) System.out.println("password does not match, try again");
                 }while(!pwd.equals(pwdCheck));
                 client.createUser(username,pwd);
-                System.out.println("Please login.");
+                System.out.println("Please login.\n");
             }
         }
 
