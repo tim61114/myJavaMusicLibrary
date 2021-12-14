dfeb094a5b29556668a58b0834ced3b07c0750ab
Tim Fan <tim61114@gmail.com>
2021-12-13 16:04:07 -0800

    added addNewAlbumToDB and showBIO method

--- src/Library.java
+++ src/Library.java
@@ -13,6 +13,10 @@ public class Library {
     List<Song> nowPlayingList = new ArrayList<>();
     int nowPlayingIndex = -1;
 
+    String currentArtistBio;
+    String currentAlbumBio;
+    String currentSongBio;
+
     public Library(MusicDB mDB){
         database = mDB;
         songs = mDB.readSongs();
@@ -93,6 +97,24 @@ public class Library {
         }
     }
 
+    public void showBio(){
+        if(nowPlaying != null){
+            Parser p = new Parser();
+            currentSongBio = p.getSongDescription(nowPlaying.getArtist().getName(),nowPlaying.getName());
+            currentArtistBio = p.getArtistDescription(nowPlaying.getArtist().getName());
+            currentAlbumBio = p.getAlbumDescription(nowPlaying.getArtist().getName(),nowPlaying.getAlbum().getName());
+            System.out.println("About the song:");
+            System.out.println(currentSongBio);
+            System.out.println("\nAbout the artist:");
+            System.out.println(currentArtistBio);
+            System.out.println("\nAbout the album:");
+            System.out.println(currentAlbumBio);
+        } else {
+            System.out.println("Uh oh so quiet");
+        }
+
+    }
+
 
     //Create new playlist
 
@@ -129,10 +151,32 @@ public class Library {
         return playlists;
     }
 
-    public void importSongs(List<Song> newSongs, List<Artist> newArtists, List<Album> newAlbums ){
+    public void addNewAlbumToDB(List<Song> newSongs, String newArtist, String newAlbum){
         database.writeSongsToDB(newSongs);
-        database.writeArtistsToDB(newArtists);
-        database.writeAlbumsToDB(newAlbums);
+        if(!checkArtistExists(newArtist)) {
+            database.writeArtistToDB(newArtist);
+            artists = database.readArtists();
+        }
+        if(!checkAlbumExists(newAlbum)) {
+            database.writeAlbumToDB(newAlbum,newArtist);
+            albums = database.readAlbums();
+        }
+        songs = database.readSongs();
+        linkData();
+    }
+
+    public boolean checkArtistExists(String artistName){
+        for(Artist a: artists){
+            if(a.getName().equalsIgnoreCase(artistName)) return true;
+        }
+        return false;
+    }
+
+    public boolean checkAlbumExists(String albumName){
+        for(Album a: albums){
+            if(a.getName().equalsIgnoreCase(albumName)) return true;
+        }
+        return false;
     }
 
 }
